d7416e818228dab956a497b322746c9869098205
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-08-23 18:56:05 +0530

    feat: API requests endpoints

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "194e429dc37739eaf6644a9990183d1b";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
