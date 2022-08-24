b641ca58e5d7eb450ac20d2509765ccb4e8007d0
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-08-24 18:36:34 +0530

    feat: Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="w-full h-screen">
+      <img
+        className="hidden sm:block absolute w-full h-full object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/0f07b807-7be1-457d-be88-eb9153d5d4e9/8db04df8-caaf-4f19-8e51-9309664068b2/IN-en-20220815-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+        alt="/"
+      />
+      <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+      <div className="fixed w-full px-4 py-24 z-50">
+        <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+          <div className="max-w-[320px] mx-auto py-16">
+            <h1 className="text-3xl font-bold">Log In</h1>
+            <form className="w-full flex flex-col py-4">
+              <input
+                className="p-3 my-2 bg-gray-700 rouded"
+                type="email"
+                placeholder="Email"
+                autoComplete="email"
+              />
+              <input
+                className="p-3 my-2 bg-gray-700 rouded"
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+              />
+              <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                Log In
+              </button>
+              <div className="flex justify-between items-center text-sm text-[#b3b3b3] ">
+                <p className="cursor-default">
+                  <input className="mr-2" type="checkbox" />
+                  Remember me
+                </p>
+                <p className="cursor-pointer hover:underline ">Need Help?</p>
+              </div>
+              <p className="py-12">
+                <span className="text-[#b3b3b3] mr-1 cursor-default">
+                  New to Netflix?
+                </span>{" "}
+                <Link to="/signup">Sign Up</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
